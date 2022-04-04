4f97cddff49cf0f200d59d8179ce25e3b7db02fe
eyeso <eyesonamerica@gmail.com>
2022-04-04 21:48:06 +0200

    Dodaj pliki projektów.

---
+++ Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra1
+{
+    class Player
+    {
+        public int x, y;
+        public Player(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void update()
+        {
+            Console.WriteLine("Updating");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gra1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(10, 20);
+            Console.WriteLine("Hello World!");
+            player.update();
+            Console.ReadLine();
+        }
+    }
+}
